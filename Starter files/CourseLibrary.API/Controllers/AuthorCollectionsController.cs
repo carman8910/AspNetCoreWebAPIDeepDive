cc2c0f9931af1d8fd616ca24d5fd8c380419fb8f
Carlos Aguilar <calberto.acastillo@hotmail.com>
2024-05-07 14:40:55 -0600

    partial changes

---
+++ Starter files/CourseLibrary.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController: ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public AuthorCollectionsController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet("{}authorIds")]
+        public async Task<ActionResult<IEnumerable<AuthorForCreationDto>>> GetAuthorCollection(
+            [FromRoute] IEnumerable<Guid> authorIds)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<AuthorDto>>> CreateAuthorCollection(
+            IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            var authorEntities = mapper.Map<IEnumerable<Author>>(authorCollection);
+            foreach (var author in authorEntities)
+            {
+                courseLibraryRepository.AddAuthor(author);
+            }
+
+            await courseLibraryRepository.SaveAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Starter files/CourseLibrary.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CourseLibrary.API.Helpers
+{
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            // Our binder works only on enumerable types
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            // Get the inputted value through the value provider
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            // if that value is null or white
+
+        }
+    }
+}
